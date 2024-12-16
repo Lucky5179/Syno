92e73656af59ddc1f8f3e928b16e50c68d53f93d
0xHarry2 <0xharry517@gmail.com>
2024-12-16 00:05:09 -0500

    init

---
+++ _next/static/chunks/app/page-2663faa34abb0430.js
@@ -0,0 +1,295 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [931],
+  {
+    5440: function (e, s, t) {
+      Promise.resolve().then(t.bind(t, 76)),
+        Promise.resolve().then(t.bind(t, 320)),
+        Promise.resolve().then(t.t.bind(t, 1749, 23)),
+        Promise.resolve().then(t.t.bind(t, 5250, 23));
+    },
+    76: function (e, s, t) {
+      "use strict";
+      t.r(s),
+        t.d(s, {
+          HeroSection: function () {
+            return n;
+          },
+        });
+      var l = t(7437);
+      let a = () =>
+        (0, l.jsx)("div", {
+          className:
+            "absolute 2xl:top-[-300px] lg:left-0 left-[-560px] lg:right-0 min-w-[1512px] video-container",
+          children: (0, l.jsx)("video", {
+            className: "object-fill w-full h-full",
+            autoPlay: !0,
+            muted: !0,
+            loop: !0,
+            playsInline: !0,
+            children: (0, l.jsx)("source", {
+              src: "/video/hero-bg.mp4",
+              type: "video/mp4",
+            }),
+          }),
+        });
+      var r = t(703);
+      let i = () =>
+          (0, l.jsx)("div", {
+            className: "video-container",
+            children: (0, l.jsx)("video", {
+              autoPlay: !0,
+              muted: !0,
+              loop: !0,
+              playsInline: !0,
+              children: (0, l.jsx)("source", {
+                src: "/video/hero-chain-selector.mp4",
+                type: "video/mp4",
+              }),
+            }),
+          }),
+        x = () =>
+          (0, l.jsxs)("div", {
+            className: "lg:w-[386px] w-full mt-[25px] lg:text-left text-right",
+            children: [
+              (0, l.jsxs)("div", {
+                className: "flex justify-between",
+                children: [
+                  (0, l.jsx)(r.default, {
+                    src: "/arrow-top-right.svg",
+                    alt: "Syno arrow",
+                    width: 12,
+                    height: 12,
+                  }),
+                  (0, l.jsx)(r.default, {
+                    src: "/arrow-bottom-left.svg",
+                    alt: "Syno arrow",
+                    width: 12,
+                    height: 12,
+                  }),
+                ],
+              }),
+              (0, l.jsx)("p", {
+                className: "text-[12px] text-[#E8E8E4] uppercase leading-tight",
+                children:
+                  " Syno is a next-generation cross-VM lending market. Lend, borrow and earn from any supported ecosystem.",
+              }),
+              (0, l.jsxs)("span", {
+                className: "text-[12px] text-gradient-blue uppercase",
+                children: ["//   SOLANA support   +   [soon]", " "],
+              }),
+            ],
+          }),
+        n = () =>
+          (0, l.jsxs)("div", {
+            className: "relative 2xl:max-h-[1200px] overflow-hidden",
+            children: [
+              (0, l.jsx)(a, {}),
+              (0, l.jsx)("div", {
+                className:
+                  "absolute w-[872px] h-[731px] lg:top-[-140px] top-[-140px] lg:right-[90px] lg:left-auto left-[-140px]",
+                children: (0, l.jsx)(r.default, {
+                  src: "/hero-atmosphere.svg",
+                  alt: "Syno hero atmosphere",
+                  fill: !0,
+                }),
+              }),
+              (0, l.jsx)("div", {
+                className: "max-w-[1600px] m-auto",
+                children: (0, l.jsxs)("div", {
+                  className:
+                    "relative z-10 px-[20px] pt-[45px] md:pb-[160px] pb-[45px] flex flex-col justify-between",
+                  children: [
+                    (0, l.jsx)("div", {
+                      className:
+                        "absolute left-0 right-0 2xl:h-[350px] lg:h-[280px] h-[80px] lg:top-[250px] top-[200px]",
+                      children: (0, l.jsx)(r.default, {
+                        src: "/hero-dotted-line.svg",
+                        alt: "Syno hero dotted line",
+                        fill: !0,
+                      }),
+                    }),
+                    (0, l.jsxs)("div", {
+                      className: "flex justify-between md:flex-row flex-col",
+                      children: [
+                        (0, l.jsxs)("div", {
+                          className:
+                            "slider-wrapper lg:h-[86px] h-[48px] lg:w-[780px] w-full",
+                          children: [
+                            (0, l.jsx)("div", {
+                              className: "slider-text",
+                              children: (0, l.jsxs)("div", {
+                                className: "flex",
+                                children: [
+                                  (0, l.jsx)("h1", {
+                                    className:
+                                      "text-gradient-white lg:text-[86px] text-[48px] uppercase leading-none",
+                                    children: " Cross_VM ",
+                                  }),
+                                  (0, l.jsx)("span", {
+                                    className:
+                                      "text-[13px] text-[#E8E8E4] lg:mt-[28px] mt-[12px] font-medium uppercase",
+                                    children: " soon ",
+                                  }),
+                                ],
+                              }),
+                            }),
+                            (0, l.jsx)("div", {
+                              className: "slider-text",
+                              children: (0, l.jsx)("div", {
+                                className: "flex",
+                                children: (0, l.jsx)("h1", {
+                                  className:
+                                    "text-gradient-white lg:text-[86px] text-[48px] uppercase leading-none",
+                                  children: " Cross_Chain ",
+                                }),
+                              }),
+                            }),
+                          ],
+                        }),
+                        (0, l.jsxs)("div", {
+                          className:
+                            "flex gap-[12px] xl:flex-row flex-col self-end",
+                          children: [
+                            (0, l.jsx)("h1", {
+                              className:
+                                "text-gradient-white lg:text-[86px] text-[48px] uppercase leading-none",
+                              children: " Lending ",
+                            }),
+                            (0, l.jsx)("div", {
+                              className: "md:block hidden",
+                              children: (0, l.jsx)(x, {}),
+                            }),
+                          ],
+                        }),
+                      ],
+                    }),
+                    (0, l.jsx)("div", {
+                      className: "md:hidden block",
+                      children: (0, l.jsx)(x, {}),
+                    }),
+                    (0, l.jsx)("div", {
+                      className:
+                        "flex flex-col gap-[16px] xl:mt-[220px] mt-[80px]",
+                      children: (0, l.jsxs)("div", {
+                        className: "self-end",
+                        children: [
+                          (0, l.jsx)("h1", {
+                            className:
+                              "text-gradient-white lg:text-[86px] text-[64px] uppercase leading-none",
+                            children: " Money_ ",
+                          }),
+                          (0, l.jsx)("h1", {
+                            className:
+                              "text-gradient-white lg:text-[86px] text-[64px] uppercase leading-none",
+                            children: " Market ",
+                          }),
+                        ],
+                      }),
+                    }),
+                  ],
+                }),
+              }),
+              (0, l.jsx)("div", {
+                className:
+                  "absolute bottom-0 left-1/2 transform -translate-x-1/2",
+                children: (0, l.jsx)(i, {}),
+              }),
+            ],
+          });
+    },
+    320: function (e, s, t) {
+      "use strict";
+      t.r(s),
+        t.d(s, {
+          PartnerCarousel: function () {
+            return i;
+          },
+        });
+      var l = t(7437);
+      t(8194);
+      var a = t(703),
+        r = t(7141);
+      let i = () =>
+        (0, l.jsxs)("div", {
+          className: "relative lg:w-[650px] w-[350px] mt-[50px] mb-[35px]",
+          children: [
+            (0, l.jsx)("div", {
+              className:
+                "partners-gradient w-[80px] absolute left-0 top-0 h-[26px] z-30 ml-[-2px]",
+            }),
+            (0, l.jsx)("div", {
+              className:
+                "partners-gradient-right w-[80px] absolute right-0 top-0 h-[30px] z-30 mr-[-2px]",
+            }),
+            (0, l.jsxs)(r.Z, {
+              speed: 22,
+              loop: 0,
+              autoFill: !0,
+              children: [
+                (0, l.jsx)("div", {
+                  className: "w-[95px] mx-[16px]",
+                  children: (0, l.jsx)(a.default, {
+                    src: "/partners/circle.svg",
+                    alt: "Syno partner Circle",
+                    width: 95,
+                    height: 26,
+                  }),
+                }),
+                (0, l.jsx)("div", {
+                  className: "w-[136px] mx-[16px]",
+                  children: (0, l.jsx)(a.default, {
+                    src: "/partners/wormhole.svg",
+                    alt: "Syno partner Wormhole",
+                    width: 136,
+                    height: 24,
+                  }),
+                }),
+                (0, l.jsx)("div", {
+                  className: "w-[72px] mx-[16px]",
+                  children: (0, l.jsx)(a.default, {
+                    src: "/partners/pyth.svg",
+                    alt: "Syno partner Pyth",
+                    width: 72,
+                    height: 24,
+                  }),
+                }),
+                (0, l.jsx)("div", {
+                  className: "w-[85px] mx-[16px]",
+                  children: (0, l.jsx)(a.default, {
+                    src: "/partners/ottersec.svg",
+                    alt: "Syno partner Ottersec",
+                    width: 85,
+                    height: 24,
+                  }),
+                }),
+                (0, l.jsx)("div", {
+                  className: "w-[122px] mx-[16px]",
+                  children: (0, l.jsx)(a.default, {
+                    src: "/partners/runtime-verification.svg",
+                    alt: "Syno partner Runtime Verification",
+                    width: 122,
+                    height: 24,
+                  }),
+                }),
+                (0, l.jsx)("div", {
+                  className: "w-[107px] mx-[16px]",
+                  children: (0, l.jsx)(a.default, {
+                    src: "/partners/gauntlet.svg",
+                    alt: "Syno partner Gauntlet",
+                    width: 107,
+                    height: 26,
+                  }),
+                }),
+              ],
+            }),
+          ],
+        });
+    },
+  },
+  function (e) {
+    e.O(0, [412, 971, 69, 744], function () {
+      return e((e.s = 5440));
+    }),
+      (_N_E = e.O());
+  },
+]);
